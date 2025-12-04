3c61d3f56670c96a49cb6603fed1e7b3597bbc33
Aaron Ellington <aaron@ellington.io>
2025-12-04 08:43:31 -0500

    day 4 part 2

--- 2025/day04/day04_test.go
+++ 2025/day04/day04_test.go
@@ -12,6 +12,9 @@ func testSuite() aoc.TestSuite {
 		SolutionPart1: day04.Part1{},
 		Part1Example:  13,
 		Part1:         1457,
+		SolutionPart2: day04.Part2{},
+		Part2Example:  43,
+		Part2:         8310,
 	}
 }
 
@@ -21,4 +24,12 @@ func Test_Part1_Example(t *testing.T) {
 
 func Test_Part1(t *testing.T) {
 	testSuite().TestPart1(t)
+}
+
+func Test_Part2_Example(t *testing.T) {
+	testSuite().TestPart2Example(t)
+}
+
+func Test_Part2(t *testing.T) {
+	testSuite().TestPart2(t)
 }
---
+++ 2025/day04/part2.go
@@ -0,0 +1,32 @@
+package day04
+
+import (
+	"github.com/aaronellington/advent-of-code/internal/aoc"
+)
+
+type Part2 struct{}
+
+func (s Part2) SolveFile() aoc.FileSolver {
+	return func(lines []string) int {
+		retrievedRollCount := 0
+
+		grid := buildGrid(lines)
+		for {
+			accessibleRollVectors := accessibleRolls(grid)
+			if len(accessibleRollVectors) == 0 {
+				break
+			}
+
+			for _, vector := range accessibleRollVectors {
+				grid[vector] = false
+				retrievedRollCount++
+			}
+		}
+
+		return retrievedRollCount
+	}
+}
+
+func (s Part2) SolveLine() aoc.LineSolver {
+	return nil
+}
