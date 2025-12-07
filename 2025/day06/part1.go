e24f494cd0048372fd3f57e2799a746149f942c6
Aaron Ellington <aaron@ellington.io>
2025-12-07 15:55:06 -0500

    clean up

--- 2025/day06/part1.go
+++ 2025/day06/part1.go
@@ -10,7 +10,6 @@ func Part1(lines []string) int {
 	values := [][]int{}
 	for _, line := range lines {
 		lineNumbers := []int{}
-
 		for _, numberString := range strings.Fields(line) {
 			number, err := strconv.Atoi(numberString)
 			if err != nil {
@@ -27,16 +26,13 @@ func Part1(lines []string) int {
 
 	grandTotal := 0
 	for i, operation := range strings.Fields(lines[len(lines)-1]) {
-
 		columnResult := values[0][i]
-
 		for _, row := range values[1:] {
 
 			switch operation {
 			case "+":
 				columnResult = columnResult + row[i]
 			case "*":
-
 				columnResult = columnResult * row[i]
 			}
 		}
