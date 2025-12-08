bae11da896beb8668821f8eaec2e7ef984f8162e
Aaron Ellington <aaron@ellington.io>
2025-12-08 08:29:15 -0500

    clean up

--- internal/aoc/solution.go
+++ internal/aoc/solution.go
@@ -8,7 +8,7 @@ type Solution func(lines []string) int
 
 func Solve(solution Solution, filePath string) int {
 	split := func(r rune) bool {
-		return r == '\n' || r == ','
+		return r == '\n'
 	}
 
 	lines := strings.FieldsFunc(readFile(filePath), split)
